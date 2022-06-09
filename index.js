414265f450baf909dec0f1c56db11dcccc5317a1
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-10 01:04:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0, 2)
+};
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier){
+    return function(){
+        return fareMultiplier * 5
+    }
+};
+
+function fareDoubler(doublesFare){
+    return doublesFare * 2
+};
+
+function fareTripler(triplesFare){
+    return triplesFare * 3
+};
+
+function selectDifferentDrivers(arrayOfDrivers, driversToReturn){
+    return driversToReturn(arrayOfDrivers)
+};
